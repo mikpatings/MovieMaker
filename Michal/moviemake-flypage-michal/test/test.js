d5186f56aacf6f4a2d78481fb1eab6fcd08e7000
Michal Waskowski <102468@glr.nl>
2025-06-18 12:15:11 +0200

    Add files via upload

---
+++ Michal/moviemake-flypage-michal/test/test.js
@@ -0,0 +1,27 @@
+const ballContainer = document.getElementById("ballContainer");
+
+function spawnBalls() {
+    for (let i = 0; i < 15; i++) {
+        createBall();
+    }
+}
+
+function createBall() {
+    const ball = document.createElement("div");
+    ball.classList.add("ball");
+
+    const x = Math.random() * (ballContainer.clientWidth - 20);
+    const y = Math.random() * (ballContainer.clientHeight - 20);
+
+    ball.style.left = `${x}px`;
+    ball.style.top = `${y}px`;
+
+    ballContainer.appendChild(ball);
+
+    setTimeout(() => {
+        ball.remove();
+    }, 5000);
+}
+
+setInterval(spawnBalls, 1000);
+
